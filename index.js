59763f41e3e3a0c136e5c9ceb3670e25762e35a1
Kumar Sanket <sanketsahu@Sankets-MacBook.blr-office.geekyants.com>
2019-04-08 20:39:36 +0530

    Fixed the node environment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var argv = require("minimist")(process.argv.slice(2));
+#!/usr/bin/env node
+
+var minimist = require("minimist");
+var argv = minimist(process.argv.slice(2));
 
 if (argv._ && argv._[0] == "init" && argv._[1]) {
   projectName = argv._[1];
